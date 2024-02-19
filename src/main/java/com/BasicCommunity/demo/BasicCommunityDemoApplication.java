69e656a2998a5fd827687c121b4e20cc40d1f8f6
KimHongHee <qaz101044@naver.com>
2024-02-19 19:16:40 +0900

    init

---
+++ src/main/java/com/BasicCommunity/demo/BasicCommunityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.BasicCommunity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCommunityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCommunityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BasicCommunity/demo/BasicCommunityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BasicCommunity.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCommunityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
