09b9641793bf02543d133f249dc893acf5ba3639
KimHongHee <qaz101044@naver.com>
2024-02-20 20:24:08 +0900

    feat : user Entity 및 Authority Enum 클래스 생성

---
+++ src/main/java/com/BasicCommunity/demo/community/entity/user/Authority.java
@@ -0,0 +1,6 @@
+package com.BasicCommunity.demo.community.entity.user;
+
+public enum Authority {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/com/BasicCommunity/demo/community/entity/user/User.java
+++ src/main/java/com/BasicCommunity/demo/community/entity/user/User.java
@@ -1,2 +1,50 @@
-package com.BasicCommunity.demo.community.entity.user;public class User {
+package com.BasicCommunity.demo.community.entity.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    // null값 없음(해당 열에 빈 값 x) , 해당 열의 값이 고유함(동일 값을 가지지 않음)
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    // @JsonIgnore다음 메서드가 JSON 직렬화 및 역직렬화 과정에서 무시되도록 지시함.
+    @JsonIgnore
+    @Column(nullable = false)
+    private String  password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    // enum 이름을 DB에 저장
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public User(String username, String password, Authority authority) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.authority = authority;
+    }
+
+
 }
